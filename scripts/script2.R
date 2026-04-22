b0020576b4add17513269df385fc24e54c6f5c03
Mark Andrews <mjandrews.org@gmail.com>
2026-04-22 15:12:34 +0100

    Add script2.R

---
+++ scripts/script2.R
@@ -0,0 +1,41 @@
+library(torch)
+
+# images are 28 by 28
+
+# define the network
+# mlp: multilayer perceptron
+mlp <- nn_sequential(
+  nn_flatten(),
+  nn_linear(784, 128), # input to hidden layer 1
+  nn_relu(),
+  nn_linear(128, 64),  # hidden layer 1 to hidden layer 2
+  nn_relu(),
+  nn_linear(64, 10) # hidden layer 2 to output
+)
+
+
+# Get MNIST data ---------------------------------------------------------
+
+library(torchvision)
+train_dataset <- mnist_dataset(
+  root = 'data', train = TRUE, download = TRUE,
+  transform = transform_to_tensor)
+
+s <- as.matrix(train_dataset[[4567]]$x[1,])
+grid::grid.raster(s, interpolate = F)
+train_dataset[[4567]]$y
+
+criterion <- nn_cross_entropy_loss()
+optimizer <- optim_adam(mlp$parameters)
+
+train_loader <- dataloader(train_data, batch_size = 64, shuffle = TRUE)
+
+epochs <- 5
+for (epoch in seq_len(epochs)){
+  for (batch in train_loader){
+    optimizer$zero_grad() # zero the gradient
+    loss <- criterion(mlp(batch$x), batch$y) # STEP 1: CALCULATE THE LOSS FUNCTION
+    loss$backward() # gradient calculation: Autograd. STEP 2: CALCULATE THE GRADIENT
+    optimizer$step() # step following the gradient. STEP 3. CHANGE THE PARAMETERS
+  }
+}
